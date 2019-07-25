c51ff3366f47a5224a95b62914c6f329fa3305bd
Juan Pablo Vargas Rodríguez <jpvargas59@gmail.com>
2019-07-25 11:11:38 -0500

    fixed reviewPage and type resenia

--- src/app/pages/review/review.page.ts
+++ src/app/pages/review/review.page.ts
@@ -14,14 +14,14 @@ export class ReviewPage implements OnInit {
   PS: Observable<resenia>;
   constructor(private db: DatabaseService, private router: Router, private route: ActivatedRoute) { }
 
-  //link = this.router.getCurrentNavigation()
-  idItem = this.route.snapshot.params['idReview'];
-  
+  // link = this.router.getCurrentNavigation()
+  idItem = this.route.snapshot.params.idReview;
+
   ngOnInit() {
     console.log(this.idItem);
     this.PS = this.db.getItem(this.idItem);
     console.log(this.PS);
-    this.db.getItem(this.idItem).subscribe( item => console.log(item))
+    this.db.getItem(this.idItem).subscribe( item => console.log(item));
   }
 
 }
--- src/app/types/resenia.ts
+++ src/app/types/resenia.ts
@@ -7,4 +7,5 @@ export interface resenia {
     puntuacion: number;
     resenia: string;
     nombre: string;
+    id?: string;
 }
